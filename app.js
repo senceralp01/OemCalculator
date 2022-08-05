fbc69178508c6753e123fff231ddeb0020e549c7
sencer2022 <senceralpdogdu@gmail.com>
2022-08-05 13:33:32 +0300

    Created modules

---
+++ app.js
@@ -0,0 +1,20 @@
+
+// Storage Controller (Module)
+const StorageController = (function() {
+
+})(); // IIFE (Immediately Invoked Function Expression)
+
+// Product Controller (Module)
+const ProductController = (function() {
+
+})();
+
+// UI Controller (Module)
+const UIController = (function() {
+
+})();
+
+// App Controller (Module)
+const AppController (function(ProdcutCtrl, UICtrl){
+
+})(ProductController, UIController);
