b52f27b4079894870cf0346006f2460675c51aeb
sencer2022 <senceralpdogdu@gmail.com>
2022-08-05 14:13:45 +0300

    Created data structure

--- app.js
+++ app.js
@@ -6,6 +6,34 @@ const StorageController = (function() {
 
 // Product Controller (Module)
 const ProductController = (function() {
+    
+    //private
+    const Product = function (id, name, price){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    const data = {
+        products: [
+            {id:0, name:'Monitor', price: 100},
+            {id:0, name:'Ram', price: 30},
+            {id:0, name:'Klavye', price: 10}
+        ],
+        selectedProduct: null,
+        totalPrice: 0
+    }
+
+    //public
+    return{
+        getProducts: function(){
+            return data.products;
+        },
+        getData: function (){
+            return data;
+        }
+    }
+
 
 })();
 
@@ -15,6 +43,18 @@ const UIController = (function() {
 })();
 
 // App Controller (Module)
-const AppController (function(ProdcutCtrl, UICtrl){
+const App = (function(ProductCtrl, UICtrl){
 
+    //public
+    return{
+        init: function(){
+            console.log("Starting App..");
+            const products = ProductCtrl.getProducts();
+            
+            UICtrl.createProductList(products);
+
+        }
+    }
 })(ProductController, UIController);
+
+App.init();
